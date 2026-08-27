4467c69d30ad3cc748744f157e168cae47fe5cc2
eXPerience83 <16572400+eXPerience83@users.noreply.github.com>
2026-08-27 15:10:49 +0200

    feat: export provider parsers

---
+++ src/news_bulletin_playlist/providers/__init__.py
@@ -0,0 +1,7 @@
+from news_bulletin_playlist.providers.cnn import CnnParser
+from news_bulletin_playlist.providers.cope import CopeParser
+from news_bulletin_playlist.providers.ondacero import OndaCeroParser
+from news_bulletin_playlist.providers.rne import RneParser
+from news_bulletin_playlist.providers.ser import SerParser
+
+__all__ = ["CnnParser", "CopeParser", "OndaCeroParser", "RneParser", "SerParser"]
