953f6d3620cbbb65f16b59b442a457a832d848c1
eXPerience83 <16572400+eXPerience83@users.noreply.github.com>
2026-09-04 22:35:43 +0200

    fix: register RFI Journal Monde parser

--- src/news_bulletin_playlist/providers/__init__.py
+++ src/news_bulletin_playlist/providers/__init__.py
@@ -1,7 +1,15 @@
 from news_bulletin_playlist.providers.cnn import CnnParser
 from news_bulletin_playlist.providers.cope import CopeParser
 from news_bulletin_playlist.providers.ondacero import OndaCeroParser
+from news_bulletin_playlist.providers.rfi import RfiJournalMondeParser
 from news_bulletin_playlist.providers.rne import RneParser
 from news_bulletin_playlist.providers.ser import SerParser
 
-__all__ = ["CnnParser", "CopeParser", "OndaCeroParser", "RneParser", "SerParser"]
+__all__ = [
+    "CnnParser",
+    "CopeParser",
+    "OndaCeroParser",
+    "RfiJournalMondeParser",
+    "RneParser",
+    "SerParser",
+]
